067649a51b174efcd305e9b1aa4d29039f36c5a2
albinantony7093-arch <albinantony7093@gmail.com>
2026-04-20 13:25:11 +0530

    dashboard updation

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,4 @@
-import 'package:empowered_ai/src/presentation/screens/home_dashboard/home_ugpg.dart';
-import 'package:empowered_ai/src/presentation/screens/landing/landing_page.dart';
+import 'package:empowered_ai/src/presentation/screens/landingpage/landing_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:get/get.dart';
@@ -13,8 +12,7 @@ class EmpowerdAi extends StatelessWidget {
       title: 'EmpoweredAi',
       debugShowCheckedModeBanner: false,
       builder: EasyLoading.init(),
-      // home: LandingPage(),
-      home: DashboardPage(),
+      home: LandingPage(),
     );
   }
 }
